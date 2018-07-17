efb2df1e8c504798a8a9f24428efcc488b648c02
Foxscotch <foxscotch@gmail.com>
2018-07-17 17:01:15 -0500

    Add blocks file

---
+++ blocks.lua
@@ -0,0 +1,9 @@
+Class = require('ext.hump.class')
+
+
+Block = Class{
+    init = function(self, pos, size)
+        self.pos = pos  -- hump.vector
+        self.size = size  -- hump.vector
+    end
+}
