b90ca8a5b7611acede58df99e9d2f89c8301c123
Foxscotch <foxscotch@gmail.com>
2018-07-19 16:54:13 -0500

    Update default resolution

--- conf.lua
+++ conf.lua
@@ -1,7 +1,10 @@
 function love.conf(t)
     t.window.title = '10x10'
+    t.window.width = 896
+    t.window.height = 504
     t.window.vsync = 0
     t.window.msaa = 4
+    
     t.modules.joystick = false
     t.modules.physics = false
 end
