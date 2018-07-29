3092d8968315285b1435c8acdceb9c9128ec3cc4
Foxscotch <foxscotch@gmail.com>
2018-07-28 23:31:44 -0400

    Add padding rendering

--- block.lua
+++ block.lua
@@ -10,10 +10,10 @@ local Block = Node:extend()
 
 
 -- Base size of blocks,
-Block.BASE_SIZE = 32
+Block.BASE_SIZE = 38
 
 -- Block spacing factor
-Block.SPACING_FACTOR = 0.06
+Block.SPACING_FACTOR = 0.07
 
 -- Default Block spacing
 Block.DEFAULT_SPACING = Block.BASE_SIZE * Block.SPACING_FACTOR
@@ -52,12 +52,15 @@ function Block:update()
 end
 
 function Block:draw()
+    local padding = self.state.padding * self.size.w
+    local size = self.size.w - padding
+    local radius = size / Block.RADIUS_DIVISOR
     deep.queue(self.onTop and 2 or 1, function ()
         love.graphics.setColor(Game.theme[self.color])
         love.graphics.rectangle('fill',
-                                self.pos.x, self.pos.y,
-                                self.size.w, self.size.h,
-                                self.size.w / Block.RADIUS_DIVISOR)
+                                self.pos.x + padding, self.pos.y + padding,
+                                size, size,
+                                radius)
     end)
 end
 
